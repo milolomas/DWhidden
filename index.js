a4c05d0fb1f29f5f4917e33b2a908fbe69ea1297
milolomas <alejandogodoy22@gmail.com>
2021-06-06 17:38:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+class dogFood {
+  constructor(marca, precio, sabor, clasificaccion) {
+    this.marca = marca;
+    this.precio = precio;
+    this.sabor = sabor;
+    this.clasificaccion = clasificaccion;
+  }
+}
+
+class catFood {
+  constructor(marca, precio, sabor, clasificaccion) {
+    this.marca = marca;
+    this.precio = precio;
+    this.sabor = sabor;
+    this.clasificaccion = clasificaccion;
+  }
+}
+
+class dogToys {
+  constructor(marca, precio,durabiidad) {
+    this.marca = marca;
+    this.precio = precio;
+    this.durabiidad = durabiidad;
+  }
+}
+
+class catToys {
+  constructor(marca, precio,durabiidad) {
+    this.marca = marca;
+    this.precio = precio;
+    this.durabiidad = durabiidad;
+  }
+}
+
+class dogHealth {
+  constructor(precio, edad, tipo) {
+    this.precio = precio;
+    this.edad = edad;
+    this.tipo = tipo;
+  }
+}
+
+class catHealth {
+  constructor(precio, edad, tipo) {
+    this.precio = precio;
+    this.edad = edad;
+    this.tipo = tipo;
+  }
+}
+
+class dogAccessories {
+  constructor(tamaño, peso, raza) {
+    this.tamaño = tamaño;
+    this.peso = peso;
+    this.raza = raza;
+  }
+}
+
+class catAccessories {
+  constructor(tamaño, peso, raza) {
+    this.tamaño = tamaño;
+    this.peso = peso;
+    this.raza = raza;
+  }
+}
